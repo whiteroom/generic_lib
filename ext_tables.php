fd1849709eb085a9a816941938428cd0bc015be1
whiteroom <schoeneck@whiteroom.cc>
2018-05-15 18:10:42 +0200

    Moving to TYPO3 CMS 8.7 LTS

--- ext_tables.php
+++ ext_tables.php
@@ -3,4 +3,5 @@ if (!defined('TYPO3_MODE')) {
 	die('Access denied.');
 }
 
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('fluid_styled_content', 'Configuration/TypoScript', 'Fluid Content Elements');
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'whiteRoom');
