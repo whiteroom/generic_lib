3e5a9360a19a3560a54fd9ea516716b79ba865ea
whiteroom <schoeneck@whiteroom.cc>
2018-05-16 14:36:35 +0200

    Version 2.0.2
    <header> is for CE only now
    Cropping with custom aspect ratio

--- Configuration/TCA/Overrides/sys_file_reference.php
+++ Configuration/TCA/Overrides/sys_file_reference.php
@@ -6,23 +6,10 @@ defined('TYPO3_MODE') or die();
  */
 
 
-/*
 $GLOBALS['TCA']['sys_file_reference']['columns']['crop']['config']['ratios'] = array(
-    '0.75' => 'Research Group 3:4',
-    '1' => 'LLL:EXT:lang/locallang_wizards.xlf:imwizard.ratio.1_1',
-    '1.3333333333333333' => 'LLL:EXT:lang/locallang_wizards.xlf:imwizard.ratio.4_3',
-    '1.7777777777777777' => 'LLL:EXT:lang/locallang_wizards.xlf:imwizard.ratio.16_9',
-    'NaN' => 'LLL:EXT:lang/locallang_wizards.xlf:imwizard.ratio.free',
+    '0.75' => 'Portrait Mode 3:4',
+    '1' => 'LLL:EXT:lang/Resources/Private/Language/locallang_wizards.xlf:imwizard.ratio.1_1',
+    '1.3333333333333333' => 'LLL:EXT:lang/Resources/Private/Language/locallang_wizards.xlf:imwizard.ratio.4_3',
+    '1.7777777777777777' => 'LLL:EXT:lang/Resources/Private/Language/locallang_wizards.xlf:imwizard.ratio.16_9',
+    'NaN' => 'LLL:EXT:lang/Resources/Private/Language/locallang_wizards.xlf:imwizard.ratio.free',
 );
-
-
-
-$GLOBALS['TCA']['sys_file_reference']['columns']['crop']['config']['ratios'] = array(
-    '1.7777777777777777' => 'LLL:EXT:lang/locallang_wizards.xlf:imwizard.ratio.16_9',
-    '1.3333333333333333' => 'LLL:EXT:lang/locallang_wizards.xlf:imwizard.ratio.4_3',
-    '0.75' => '3:4',
-    '1' => 'LLL:EXT:lang/locallang_wizards.xlf:imwizard.ratio.1_1',
-    'NaN' => 'LLL:EXT:lang/locallang_wizards.xlf:imwizard.ratio.free',
-);
-
-*/
