ad1a47e6bd9b0040254eea917d7983c3835a3e6d
whiteroom <schoeneck@whiteroom.cc>
2018-08-16 17:05:52 +0200

    Cleanup 8.7

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -3,4 +3,3 @@ if (!defined('TYPO3_MODE')) {
         die ('Access denied.');
 }
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes('tt_content','subheader', 'text,textpic,textmedia,html,image', 'before:bodytext');
