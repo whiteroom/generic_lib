4fd92dc8a6fa3b35faea63b12497090215356d8c
Daniel Schoeneck <whiteroom@users.noreply.github.com>
2019-05-23 18:21:23 +0200

    Allow longer subheaders (limited to 256 chars)
    Update extension, add ext_tables.sql

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -3,3 +3,7 @@ if (!defined('TYPO3_MODE')) {
         die ('Access denied.');
 }
 
+$GLOBALS['TCA']['tt_content']['columns']['subheader']['config']['max']= 4096;
+$GLOBALS['TCA']['tt_content']['columns']['subheader']['config']['type']= 'text';
+$GLOBALS['TCA']['tt_content']['columns']['subheader']['config']['cols']= 40;
+$GLOBALS['TCA']['tt_content']['columns']['subheader']['config']['rows']= 3;
