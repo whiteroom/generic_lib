0265428e48a0fa3d3dba7be50ec35a56060113d3
whiteroom <mail@whiteroom.cc>
2019-07-26 15:48:51 +0200

    allow filelink sorting ASC and DESC, Fixes #32

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -3,7 +3,39 @@ if (!defined('TYPO3_MODE')) {
         die ('Access denied.');
 }
 
+// allow longer subheaders
 $GLOBALS['TCA']['tt_content']['columns']['subheader']['config']['max']= 4096;
 $GLOBALS['TCA']['tt_content']['columns']['subheader']['config']['type']= 'text';
 $GLOBALS['TCA']['tt_content']['columns']['subheader']['config']['cols']= 40;
 $GLOBALS['TCA']['tt_content']['columns']['subheader']['config']['rows']= 3;
+
+
+// allow filelink sorting ASC and DESC
+// Add new TCA field to the table "tt_content"
+$temporaryColumns = array (
+        'filelink_sorting_direction' => array (
+
+			    'label' => 'Direction:',
+                'config' => array (
+                       'type' => 'select',
+                       'renderType' => 'selectSingle',
+                       'items' => array (
+							array('Ascending', 0),
+							array('Descending', 1),
+                       ),
+	                   'size' => 1,
+	                   'maxitems' => 1,
+	                   'eval' => '',
+				       'default' => 0
+                )
+        ),
+);
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns(
+        'tt_content',
+        $temporaryColumns
+);
+
+
+// Add the field "filelink_sorting_direction" to the TCA palette "uploads"
+ \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addFieldsToPalette('tt_content', 'uploads', 'filelink_sorting_direction', 'after:filelink_sorting');
--- ext_tables.sql
+++ ext_tables.sql
@@ -1,7 +1,8 @@
 #allow longer subheaders
+#allow filelink sorting ASC and DESC
 
 CREATE TABLE tt_content (
-
-    subheader text
+   filelink_sorting_direction tinyint(1) unsigned DEFAULT '0' NOT NULL, 
+   subheader text
    
 );
