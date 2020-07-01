397f3f2952e5dc5b32d6c07c50e4f3c2e3ee9464
whiteroom <mail@whiteroom.cc>
2020-07-01 16:34:56 +0200

    Update ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -21,19 +21,19 @@ $EM_CONF[$_EXTKEY] = array(
 	'uploadfolder' => '0',
 	'createDirs' => '',
 	'clearCacheOnLoad' => 0,
-	'version' => '4.4.0',
+	'version' => '5.0.0',
 	'constraints' => [
 	    'depends' => [
-	        'php' => '7.2.0-7.2.99',
-			'typo3' => '9.5.00-9.5.99',
-			'indexed_search' => '9.5.00-9.5.99',
-			'fluid_styled_content' => '9.5.00-9.5.99',
-			'impexp' => '9.5.00-9.5.99',
-			'news' => '7.3.0-7.3.99',
+	        'php' => '7.2.0-7.4.99',
+			'typo3' => '10.4.1-10.4.99',
+			'indexed_search' => '10.4.1-10.4.99',
+			'fluid_styled_content' => '10.4.1-10.4.99',
+			'impexp' => '10.4.1-10.4.99',
+			'news' => '8.1.1-8.3.99',
 			'rx_shariff' => '13.0.0-13.99.99',
 			'vhs' => '6.0.0-6.0.99',
 			'dp_cookieconsent' => '10.5.0-10.5.99',
-	        	'fluid-styled-responsive-images' => '10.2.0-10.2.99'
+	        'fluid-styled-responsive-images' => '10.2.0-10.2.99'
 	    ],
 	    'conflicts' => [
 
