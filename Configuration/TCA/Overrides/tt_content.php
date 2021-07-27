ae6241c9edbfa9d52c5fb1d07e6a58b2d23daaec
Daniel Schoeneck <whiteroom@users.noreply.github.com>
2021-07-27 16:47:45 +0200

    obsolete since TYPO3 Version 9
    filelink_sorting_direction is now obsolete since TYPO3 Version 9 (Feature: #65403 - Add file links sorting by date and sorting direction)

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -11,31 +11,4 @@ $GLOBALS['TCA']['tt_content']['columns']['subheader']['config']['rows']= 3;
 
 
 // allow filelink sorting ASC and DESC
-// Add new TCA field to the table "tt_content"
-$temporaryColumns = array (
-        'filelink_sorting_direction' => array (
-
-			    'label' => 'Direction:',
-                'config' => array (
-                       'type' => 'select',
-                       'renderType' => 'selectSingle',
-                       'items' => array (
-							array('Ascending', 0),
-							array('Descending', 1),
-                       ),
-	                   'size' => 1,
-	                   'maxitems' => 1,
-	                   'eval' => '',
-				       'default' => 0
-                )
-        ),
-);
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns(
-        'tt_content',
-        $temporaryColumns
-);
-
-
-// Add the field "filelink_sorting_direction" to the TCA palette "uploads"
- \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addFieldsToPalette('tt_content', 'uploads', 'filelink_sorting_direction', 'after:filelink_sorting');
+// obsolete since TYPO3 Version 9 (Feature: #65403 - Add file links sorting by date and sorting direction)
--- ext_tables.sql
+++ ext_tables.sql
@@ -2,7 +2,6 @@
 #allow filelink sorting ASC and DESC
 
 CREATE TABLE tt_content (
-   filelink_sorting_direction tinyint(1) unsigned DEFAULT '0' NOT NULL, 
    subheader text
    
 );
