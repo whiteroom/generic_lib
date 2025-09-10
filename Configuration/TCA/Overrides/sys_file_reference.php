86e19bb6946303ad7749556f26209aec3dfbb67a
Daniel Schoeneck <whiteroom@users.noreply.github.com>
2025-09-10 15:46:10 +0200

    Constant TYPO3_MODE is deprecated.

--- Configuration/TCA/Overrides/sys_file_reference.php
+++ Configuration/TCA/Overrides/sys_file_reference.php
@@ -1,5 +1,5 @@
 <?php
-defined('TYPO3_MODE') or die();
+defined('TYPO3') or die();
 
 /**
  * Changes to TYPO3 8.7 (Feature): Allow overriding cropVariants for Image Manipulation and extra cropping aspects ratios in pageTSConfig
--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -1,7 +1,5 @@
 <?php
-if (!defined('TYPO3_MODE')) {
-        die ('Access denied.');
-}
+defined('TYPO3') || die();
 
 // allow longer subheaders
 $GLOBALS['TCA']['tt_content']['columns']['subheader']['config']['max']= 4096;
