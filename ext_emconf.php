d7d325f7aec111a61ad5833f74b83fe00a3c5adc
Daniel Schoeneck <whiteroom@users.noreply.github.com>
2025-11-10 13:25:54 +0100

    Suggest the use of news_administration

--- ext_emconf.php
+++ ext_emconf.php
@@ -21,7 +21,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'uploadfolder' => '0',
 	'createDirs' => '',
 	'clearCacheOnLoad' => 0,
-	'version' => '13.0.8',
+	'version' => '13.0.9',
 	'constraints' => [
 	    'depends' => [
 	        'php' => '8.1.0-8.3.99',
@@ -40,7 +40,7 @@ $EM_CONF[$_EXTKEY] = array(
 
 	    ],
 	    'suggests' => [
-
+			'news_administration' => '2.0.0-2.0.99',
 	    ]
 	]
 );
